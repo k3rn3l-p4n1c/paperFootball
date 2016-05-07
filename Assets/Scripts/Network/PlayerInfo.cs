8fdec52d5c1f718cea2ff832199831dc50f124fb
KK <esmaielzadeh73@gmail.com>
2016-05-07 07:41:56 -0700

    player added

---
+++ Assets/Scripts/Network/PlayerInfo.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ *this class contains Player info which we get from  
+ * Server By Socket
+ */
+public class PlayerInfo {
+    
+    private int score;
+    private int rank;
+    private string name;
+    private int numberOfGoal;
+
+    public void setScore(int score)
+    {
+        this.score = score;
+    }
+    public void setRank(int rank)
+    {
+        this.rank = rank;
+    }
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public void setNumberOfGoal(int num)
+    {
+        numberOfGoal = num;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+    public int getRank()
+    {
+        return rank;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public int getNumberOfGoal()
+    {
+        return numberOfGoal;
+    }
+}
