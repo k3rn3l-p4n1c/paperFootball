11c2c34e4d9baf0bbc55faf42fe5e0430ddcd0bb
KK <esmaielzadeh73@gmail.com>
2016-05-08 08:54:13 -0700

    menu added

---
+++ Assets/Scripts/onClicks/MenuBeginBtn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuBeginBtn : MonoBehaviour {
+
+    Button myButton;
+
+    void Awake()
+    {
+        myButton = GetComponent<Button>(); 
+
+        myButton.onClick.AddListener(() => { OpenSceneOnClickEvent(); });  
+        
+    }
+
+    void OpenSceneOnClickEvent()
+    {
+        SceneManager.LoadScene("Arena");  
+       
+    }
+
+   
+}
