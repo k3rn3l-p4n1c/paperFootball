6d74cb874065bb255bfdfd7ca085f4f04c7fd4ea
Shahin Rahbari <Shahin@Shahins-MacBook-Pro.local>
2016-05-29 18:09:56 +0430

    get infos added

--- Assets/Scripts/Network/RestClient.cs
+++ Assets/Scripts/Network/RestClient.cs
@@ -53,24 +53,130 @@ public class RestClient : MonoBehaviour
             Debug.Log("error" + www.error);
         }
     }
-    IEnumerator SendingRequest(WWW www)
+
+    public void GetOpenLeags(string username)
     {
-        Debug.Log("Before yield");
+        Url = "http://www.mocky.io/v2/574acf4010000034131b2ae7";
+
+        WWW www = new WWW(Url);
+        Debug.Log("Befor Sending Req");
+        //SendingRequest(www);
+        // StartCoroutine("GetdataEnumerator",Url);
+        StartCoroutine(GetOpenLeagsdataEnumerator(www));
+    }
+    IEnumerator GetOpenLeagsdataEnumerator(WWW www)
+    {
+        //Wait for request to complete
         yield return www;
 
-        Debug.Log(www.error);
-        Debug.Log(www.text);
-
-        if (www.error == null)
+        JsonData data = JsonMapper.ToObject(www.text);
+        Debug.Log(data["leageBasie"]);
+        Debug.Log(data["leageSemiPro"]);
+        Debug.Log(data["leagePro"]);
+        if (www.error != null)
         {
-            string msg = www.text;
-            Debug.Log("»[[[[[[ۀۀۀۀۀۀ" + msg);
-
-
-
-            // ParseServerSendingValidate(www.text);
+            string serviceData = www.text;
+            //Data is in json format, we need to parse the Json.
+            Debug.Log(serviceData);
         }
-
+        else
+        {
+            Debug.Log("error" + www.error);
+        }
     }
+
+    public void GetUserInfo(string username)
+    {
+        Url = "http://www.mocky.io/v2/574ad1aa100000ad131b2ae9";
+
+        WWW www = new WWW(Url);
+        Debug.Log("Befor Sending Req");
+        //SendingRequest(www);
+        // StartCoroutine("GetdataEnumerator",Url);
+        StartCoroutine(GetUserInfoEnumerator(www));
+    }
+    IEnumerator GetUserInfoEnumerator(WWW www)
+    {
+        //Wait for request to complete
+        yield return www;
+
+        JsonData data = JsonMapper.ToObject(www.text);
+        Debug.Log(data["level"]);
+        Debug.Log(data["coin"]);
+        if (www.error != null)
+        {
+            string serviceData = www.text;
+            //Data is in json format, we need to parse the Json.
+            Debug.Log(serviceData);
+        }
+        else
+        {
+            Debug.Log("error" + www.error);
+        }
+    }
+
+    public void GetEnemyInfo()
+    {
+        Url = "http://  hossein/getEnemyInfo?username=";
+
+        WWW www = new WWW(Url);
+        Debug.Log("Befor Sending Req");
+        //SendingRequest(www);
+        // StartCoroutine("GetdataEnumerator",Url);
+        StartCoroutine(GetEnemyInfoEnumerator(www));
+    }
+    IEnumerator GetEnemyInfoEnumerator(WWW www)
+    {
+        //Wait for request to complete
+        yield return www;
+
+        JsonData data = JsonMapper.ToObject(www.text);
+        Debug.Log(data["level"]);
+        Debug.Log(data["coin"]);
+        if (www.error != null)
+        {
+            string serviceData = www.text;
+            //Data is in json format, we need to parse the Json.
+            Debug.Log(serviceData);
+        }
+        else
+        {
+            Debug.Log("error" + www.error);
+        }
+    }
+
+    public void GetLeaderBoardInfo(string username)
+    {
+        Url = "http://  hossein/getLeaderBoardInfo?username="+username;
+
+        WWW www = new WWW(Url);
+        Debug.Log("Befor Sending Req");
+        //SendingRequest(www);
+        // StartCoroutine("GetdataEnumerator",Url);
+        StartCoroutine(GetLeaderBoardInfoEnumerator(www));
+    }
+    IEnumerator GetLeaderBoardInfoEnumerator(WWW www)
+    {
+        //Wait for request to complete
+        yield return www;
+
+        JsonData data = JsonMapper.ToObject(www.text);
+        Debug.Log(data["1"]);
+        Debug.Log(data["2"]);
+        Debug.Log(data["3"]);
+        Debug.Log(data["rank"]);
+        Debug.Log(data["allPalyersNum"]);
+        if (www.error != null)
+        {
+            string serviceData = www.text;
+            //Data is in json format, we need to parse the Json.
+            Debug.Log(serviceData);
+        }
+        else
+        {
+            Debug.Log("error" + www.error);
+        }
+    }
+    
 
 }
