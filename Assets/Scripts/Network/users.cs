fdb3962af126be3e0b3213a8d00cd56e74131d0b
Shahin Rahbari <Shahin@Shahins-MacBook-Pro.local>
2016-07-04 12:07:59 +0430

    arrow fixed

---
+++ Assets/Scripts/Network/users.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class users  {
+    public string level {get;set;}
+    public string point {get;set;}
+    public string username {get;set;}
+
+	
+}
